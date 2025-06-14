bc0e71ef7c7a7c2967da09b7047936acbcc7ecb2
fichimura <fabichimura@gmail.com>
2025-06-14 20:58:11 -0300

    Feature - header com ícones de login e menu, e header com as cores iguais ao do aplicativo inteiro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'poke-header',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatTooltipModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
